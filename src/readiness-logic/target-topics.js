eeb46fec3562c9f9ea8197ea08a19a8c5bfe68c6
michaeltreynolds <michaeltreynolds@gmail.com>
2025-04-08 21:12:46 -0600

    Ensure targetTopics is explicitly ordered to match targetTopicQuestionTarget initialization

--- src/readiness-logic/target-topics.js
+++ src/readiness-logic/target-topics.js
@@ -12,5 +12,16 @@ export const targetTopicQuestionTarget = {
     'queue': 5,
 };
 
-
-export const targetTopics = Object.keys(targetTopicQuestionTarget);
+export const targetTopics = [
+    'hash-table',
+    'string',
+    'linked-list',
+    'array',
+    'depth-first-search',
+    'breadth-first-search',
+    'binary-search',
+    'dynamic-programming',
+    'sorting',
+    'heap-priority-queue',
+    'queue',
+];
